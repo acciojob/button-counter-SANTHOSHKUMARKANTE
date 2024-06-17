3f31468582500f1009499b5508513d57977e250f
Santhosh <kantesanthun123@gmail.com>
2024-06-17 17:21:04 +0530

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count,setCount] = useState(0);
+  const increment = () => {
+    setCount(count+1);
+  }
   return (
     <div>
-        <h1>Hello</h1>
+        <p>Button clicked {count} times</p>
+        <button onClick={increment}>Click me</button>
     </div>
   )
 }
